51c271d713cf9cf6ce36be4c66bba8c48548b867
Maksim Grishenko <endtimes104@gmail.com>
2018-09-20 01:49:44 +0300

    change path panel index file

--- routes/admin/routes.php
+++ routes/admin/routes.php
@@ -1,5 +1,5 @@
 <?php
 
 Flight::route('/admin', function(){
-	Flight::render('../templates/admin/index', array('title' => 'Панель администратора'));
+	Flight::render('panel/index', array('title' => 'Панель администратора'));
 });
