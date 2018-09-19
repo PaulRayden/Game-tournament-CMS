1352cd52bdda14a9c47103296aab7fcb4bf8da28
Maksim Grishenko <endtimes104@gmail.com>
2018-09-20 01:41:34 +0300

    add index panel file

---
+++ templates/panel/index.php
@@ -0,0 +1,8 @@
+<?php Flight::render('layouts/header', array('title' => $title)); ?>
+<body>
+	<h3 class="text" style="text-align: center;">Flight Framework</h3>
+	<h3 class="text-2" style="text-align: center;">Start Page</h3>
+	<a class="link" target="_blank" href="http://flightphp.com/">Official page</a>
+	<a class="link" target="_blank" href="http://flightphp.com/learn/">Docs</a>
+</body>
+<?php Flight::render('layouts/footer'); ?>
