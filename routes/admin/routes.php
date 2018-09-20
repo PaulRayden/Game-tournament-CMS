ccf29cd672e86341400d94740a6493e2ce135693
Maksim Grishenko <endtimes104@gmail.com>
2018-09-20 08:55:58 +0300

    added route for panel login

--- routes/admin/routes.php
+++ routes/admin/routes.php
@@ -3,3 +3,7 @@
 Flight::route('/panel', function(){
 	Flight::render('panel/index', array('title' => 'Панель администратора'));
 });
+
+Flight::route('/panel/login', function(){
+	Flight::render('panel/login', array('title' => 'Авторизация - Панель администратора'));
+});
