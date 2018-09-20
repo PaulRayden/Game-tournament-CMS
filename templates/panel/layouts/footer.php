3411e6a1a9576d2d14af7bceb987bc9a10616e90
Maksim Grishenko <endtimes104@gmail.com>
2018-09-20 09:02:12 +0300

    fix path to css and js files

--- templates/panel/layouts/footer.php
+++ templates/panel/layouts/footer.php
@@ -1,5 +1,8 @@
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript" src="/js/mb.js">
+
+</script>
   <script>
     <?php include(__DIR__.'/assets/js/main.js'); ?>
   </script>
