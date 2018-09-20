6011a5835cbce9a2c052fc4259c3ae8bb1ba12fa
Maksim Grishenko <endtimes104@gmail.com>
2018-09-20 08:53:28 +0300

    create admin js file and connected to layout

--- templates/panel/layouts/footer.php
+++ templates/panel/layouts/footer.php
@@ -1,6 +1,6 @@
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-<script src="js/main.js"></script>
-
+  <script>
+    <?php include(__DIR__.'/assets/js/main.js'); ?>
+  </script>
 </html>
