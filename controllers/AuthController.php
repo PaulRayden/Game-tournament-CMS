0733d41b621c4090aea149ef4d6ee2aa2a749733
Maksim Grishenko <endtimes104@gmail.com>
2018-09-22 01:13:57 +0300

    created auth methods

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -2,11 +2,30 @@
 
 namespace Controllers;
 
-use Models\Model_Users as UsersModel;
+use Models\Model_Auth as Auth;
+
 
 class AuthController
 {
+    public static function login($email,$password)
+    {
 
-  
+    }
+    public static function register($name,$email,$password,$password_2)
+    {
 
+    }
+
+    protected static function checkName()
+    {
+
+    }
+    protected static function checkEmail()
+    {
+
+    }
+    protected static function passwordHandler()
+    {
+
+    }
 }
