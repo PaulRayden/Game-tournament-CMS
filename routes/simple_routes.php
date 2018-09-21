1f4814c66cea3ce92bfaf03fafbf43b0ff5106e9
Maksim Grishenko <endtimes104@gmail.com>
2018-09-22 00:47:22 +0300

    create route

--- routes/simple_routes.php
+++ routes/simple_routes.php
@@ -5,7 +5,15 @@
         {
             Flight::render('login', array('title' => 'Войти в систему')); 
         }else{
-            Flight::render('index', array('title' => 'Flight')); 
+            Flight::render('index', array('title' => 'Главная')); 
         }
     });
     
+    Flight::route('/register', function(){
+        if(!isset($_SESSION['user']))
+        {
+            Flight::render('register', array('title' => 'Войти в систему')); 
+        }else{
+            Flight::render('index', array('title' => 'Главная')); 
+        }
+    });
