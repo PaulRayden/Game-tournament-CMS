f2a94b0f8d160cc1ede6a476033c138b57707081
Maksim Grishenko <endtimes104@gmail.com>
2018-09-22 00:46:53 +0300

    update login file and header

--- templates/default/login.php
+++ templates/default/login.php
@@ -1,18 +1,18 @@
 <?php Flight::render('layouts/header', array('title' => $title)); ?>
-<body>
+<body class="body-login">
   <div class="container">
     <div class="card auth-panel" >
-      <h5 class="card-header info-color white-text text-center py-4">
-        <strong>Войти в систему</strong>
-      </h5>
       <div class="card-body px-lg-5 pt-0">
         <form id="login_panel-form" method="post" class="text-center">
 
-          <input type="email" class="form-control mb-4 mt-4" name="email" placeholder="E-mail">
+          <input type="email" class="form-control form-control-lg mb-4 mt-4 " name="email" placeholder="E-mail">
 
-          <input type="password" class="form-control mb-4" name="password" placeholder="Пароль">
+          <input type="password" class="form-control form-control-lg mb-4" name="password" placeholder="Пароль">
 
-          <a id="login_panel-button" class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" href="#">Войти</a>
+          <a id="login_panel-button" class="btn btn-elegant w-100" href="#">Войти через steam</a>
+          <a id="login_panel-button" class="btn btn-primary w-100" href="#">Войти</a>
+          <a class="btn btn-primary w-100" href="/register">Регистрация</a>
+
 
         </form>
       </div>
