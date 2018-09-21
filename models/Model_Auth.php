6c1f6fdbf74131e1e843fc34613c33dffb0425ab
Maksim Grishenko <endtimes104@gmail.com>
2018-09-22 02:23:50 +0300

    created methods Model_Auth

--- models/Model_Auth.php
+++ models/Model_Auth.php
@@ -8,6 +8,27 @@ use \RedBeanPHP\SimpleModel as Model;
 class Model_Auth extends Model
 {
 
-  
+  // Создание аккаунта пользователя
+  public static function createAccount($name,$email,$password)
+  {
+      $user = R::dispense('users');
+      $user->name = $name;
+      $user->email = $email;
+      $user->password = $password;
+      R::store($user);
+  }
 
+
+  //Поиск пользователя по имени в таблице users
+  public static function FindName($name)
+  {
+      return R::findOne('users','name = ?',array($name));
+  }
+
+
+  //Поиск пользователя по почте в таблице users
+  public static function FindEmail($email)
+  {
+      return R::findOne('users','email = ?',array($email));
+  }
 }
