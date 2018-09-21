9f7bb1ab78a72cb0350d2ca23564785d84893ecc
Maksim Grishenko <endtimes104@gmail.com>
2018-09-22 02:21:27 +0300

    update markup

--- templates/default/layouts/footer.php
+++ templates/default/layouts/footer.php
@@ -1,6 +1,6 @@
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript" src="/js/mb.js">
-<script type="text/javascript" src="/js/main.js">
+<script type="text/javascript" src="/js/main.js"></script>
+<script type="text/javascript" src="/js/mb.js"></script>
 
 </html>
