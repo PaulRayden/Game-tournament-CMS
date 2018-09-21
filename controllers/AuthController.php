fe26936f79a0e0b72dac19b878f98e44e64ecd60
Maksim Grishenko <endtimes104@gmail.com>
2018-09-22 02:24:31 +0300

    updated methods AuthController

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -5,27 +5,93 @@ namespace Controllers;
 use Models\Model_Auth as Auth;
 
 
-class AuthController
+class AuthController extends SystemController
 {
     public static function login($email,$password)
     {
 
     }
-    public static function register($name,$email,$password,$password_2)
+    public static function register($name,$email,$password,$password_verify)
     {
+        self::checkName($name);
+        self::checkEmail($email);
+        $hash = self::passwordHandler($password,$password_verify);
+        self::create($name,$email,$hash);
 
     }
 
-    protected static function checkName()
+    protected static function checkName($name)
     {
+
+        if ($name == ''){
+            echo self::json([
+                'message' => 'Введите имя',
+                'status'  => 'error'
+            ]);
+            exit();
+        }
+    }
+
+    protected static function checkEmail($email)
+    {
+        $user = Auth::FindEmail($email);
+
+        if ($email == '')
+        {
+            echo self::json([
+                'message' => 'Введите почту',
+                'status'  => 'error'
+            ]);
+            exit();
+        }
+        if ($user){
+            echo self::json([
+                'message' => 'Такая почта уже зарегистрирована',
+                'status'  => 'error'
+            ]);
+            exit();
+        }
+    }
+    protected static function passwordHandler($password,$password_verify)
+    {
+
+        if ($password == '')
+        {
+            echo self::json([
+                'message' => 'Введите пароль',
+                'status'  => 'error'
+            ]);
+            exit();
+        }
+        if ($password_verify == '')
+        {
+            echo self::json([
+                'message' => 'Введите пароль повторно',
+                'status'  => 'error'
+            ]);
+            exit();
+        }
+        if ($password != $password_verify)
+        {
+            echo self::json([
+                'message' => 'Пароли не совпадают',
+                'status'  => 'error'
+            ]);
+            exit();
+        }else{
+            return $password_hash = password_hash($password,PASSWORD_DEFAULT);
+        }
 
     }
-    protected static function checkEmail()
-    {
 
-    }
-    protected static function passwordHandler()
+    protected static function create($name,$email,$password)
     {
+        Auth::createAccount($name,$email,$password);
 
+        echo self::json([
+            'message' => 'Вы успежно зарегистрировались',
+            'status'  => 'success'
+        ]);
+        exit();
     }
 }
