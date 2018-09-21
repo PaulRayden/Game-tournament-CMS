3675d4a06be137ba14a5a93ff32e0e89c44882d1
Maksim Grishenko <endtimes104@gmail.com>
2018-09-22 02:22:10 +0300

    update routes and delete routes.php

--- routes/ajax_routes.php
+++ routes/ajax_routes.php
@@ -1,18 +1,19 @@
 <?php
 
-Flight::route('POST auth/login', function(){
+Flight::route('POST /auth/login', function(){
     Controllers\AuthController::login(
         $_POST['email'],
         $_POST['password']
     );
 });
-Flight::route('POST auth/register', function(){
+Flight::route('GET|POST /auth/register', function(){
+
 
     Controllers\AuthController::register(
         $_POST['name'],
         $_POST['email'],
         $_POST['password'],
-        $_POST['password_2']
+        $_POST['password_verify']
     );
 
 });
--- routes/routes.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-include_once __DIR__."\simple_routes.php";
-include_once __DIR__."\ajax_routes.php";
