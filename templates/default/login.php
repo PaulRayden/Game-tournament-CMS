cbccaec556a7752bc1554ae7a5eeb81df54f9fda
Maksim Grishenko <endtimes104@gmail.com>
2018-09-23 11:33:12 +0300

    update makup login and register view

--- templates/default/login.php
+++ templates/default/login.php
@@ -4,12 +4,11 @@
     <div class="card auth-panel" >
       <div class="card-body px-lg-5 pt-0">
         <form id="login_panel-form" method="post" class="text-center">
-          <input type="email" class="form-control form-control-lg mb-4 mt-4 " name="email" placeholder="E-mail">
+          <input type="email" class="form-control form-control-lg mb-4 mt-4 " name="email" placeholder="E-mail" required>
 
           <input type="password" class="form-control form-control-lg mb-4" name="password" placeholder="Пароль">
-
-          <a id="login_panel-button" class="btn btn-elegant w-100" href="#">Войти через steam</a>
-          <a id="login_panel-button" class="btn btn-primary w-100" >Войти</a>
+          <a id="login_panel-steam" class="btn btn-elegant w-100" href="#">Войти через steam</a>
+          <a type="submit" id="login_panel-button" class="btn btn-primary w-100" >Войти</a>
           <a class="btn btn-primary w-100" href="/register">Регистрация <i class="fa fa-arrow-right font-weight-bold" aria-hidden="true"></i></a>
 
 
--- templates/default/register.php
+++ templates/default/register.php
@@ -4,6 +4,7 @@
     <div class="card auth-panel" >
       <div class="card-body px-lg-5 pt-0">
         <form id="register_panel-form" method="post" class="text-center">
+
             <input type="text" class="form-control form-control-lg mb-4 mt-4" name="name" placeholder="Ваше имя">
             <input type="email" class="form-control form-control-lg mb-4 mt-4" name="email" placeholder="E-mail">
 
@@ -11,7 +12,7 @@
             <input type="password" class="form-control form-control-lg mb-4" name="password_verify" placeholder="Повторите пароль">
     
             <a id="register_panel-button" class="btn btn-primary w-100">Регистрация</a>
-            <a class="btn btn-primary w-100" href="/"><i class="fa fa-arrow-left font-weight-bold" aria-hidden="true"></i> Войти</a>
+            <a class="btn btn-primary w-100" href="/"><i class="fa fa-arrow-left font-weight-bold" aria-hidden="true"></i> Авторизация</a>
 
         </form>
       </div>
