6a10ece16cc508b207391d255c508ca091d45ea9
Maksim Grishenko <endtimes104@gmail.com>
2018-09-23 09:08:24 +0300

    update settings file

--- settings.php
+++ settings.php
@@ -3,4 +3,10 @@
 use \RedBeanPHP\R as R;
 
 R::setup( 'mysql:host=localhost;dbname=tournament-cms', 'root', '' );
-Flight::set('flight.views.path', '../templates/default');
+
+Flight::register('template', 'Controllers\SystemController::template');
+Flight::register('FolderTemplate', 'Controllers\SystemController::FolderTemplate');
+
+//Flight::set('flight.views.path', '../'.Flight::FolderTemplate().'/'.Flight::template());
+
+Flight::set('flight.views.path', '../templates/'.Flight::template());
