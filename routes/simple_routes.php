82f2c2917ebfcd52f16fc4f83e90975698b00a11
Maksim Grishenko <endtimes104@gmail.com>
2018-09-23 09:09:23 +0300

    add route

--- routes/simple_routes.php
+++ routes/simple_routes.php
@@ -1,19 +1,27 @@
 <?php
-    
     Flight::route('/', function(){
         if(!isset($_SESSION['user']))
         {
-            Flight::render('login', array('title' => 'Войти в систему')); 
+            Flight::redirect('/login');
         }else{
             Flight::render('index', array('title' => 'Главная')); 
         }
     });
-    
+
+    Flight::route('/login', function(){
+        if(!isset($_SESSION['user']))
+        {
+            Flight::render('login', array('title' => 'Авторизация'));
+        }else{
+            Flight::redirect('/');
+        }
+    });
+
     Flight::route('/register', function(){
         if(!isset($_SESSION['user']))
         {
-            Flight::render('register', array('title' => 'Войти в систему')); 
+            Flight::render('register', array('title' => 'Регистрация'));
         }else{
-            Flight::render('index', array('title' => 'Главная')); 
+            Flight::redirect('/');
         }
     });
