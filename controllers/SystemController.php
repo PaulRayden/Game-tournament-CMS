466dd1d53c10a7d04e5da348da02c83d25844781
Maksim Grishenko <endtimes104@gmail.com>
2018-09-23 09:11:05 +0300

    add 2 methods in SystemController

--- controllers/SystemController.php
+++ controllers/SystemController.php
@@ -2,14 +2,25 @@
 
 namespace Controllers;
 
+use Models\Model_System as SystemModel;
+
 class SystemController
 {
 
-    protected static function dump($data)
+    public static function dump($data)
     {
         echo '<pre>';
         print_r($data);
         echo '</pre>';
+    }
+
+    public static function template()
+    {
+        return SystemModel::findTemplate();
+    }
+    public static function FolderTemplate()
+    {
+        return SystemModel::findFolderTemplate();
     }
 
     protected static function json($array)
