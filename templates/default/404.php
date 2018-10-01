5e961f814072567bdd359044416c8dc829c490b6
Maksim Grishenko <endtimes104@gmail.com>
2018-10-01 16:44:11 +0300

    update 404 view

--- templates/default/404.php
+++ templates/default/404.php
@@ -1,4 +1,19 @@
-<?php Flight::render('layouts/header', array('title' => $title)); ?>
-    <body class="body-404">
-    </body>
-<?php Flight::render('layouts/footer'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $title ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/mb.css">
+    <link rel="icon" type="image/png" href="/assets/img/favicon.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700&subset=cyrillic">
+    <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+    <script src="/js/jquery.js"></script>
+</head>
+<body class="body-404">
+
+</body>
+<script type="text/javascript" src="/js/mb.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>
+</html>
