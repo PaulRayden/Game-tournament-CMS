064b63f2f270d0c832bda77a4cc7cf43c8307092
Maksim Grishenko <endtimes104@gmail.com>
2018-10-01 16:54:54 +0300

    del 2 method in PageController

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: InsideGo
- * Date: 23.09.2018
- * Time: 17:09
- */
-
 namespace Controllers;
 
 use Controllers\RoleController as RoleController;
@@ -17,16 +10,6 @@ class PageController extends RoleController
     public static function index()
     {
         self::role('admin');
-        Flight::render('panel/index', array('title' => 'Панель администратора'));
-    }
-    public function settings()
-    {
-        self::role('admin');
-        Flight::render('panel/settings', array('title' => 'Панель администратора - Настройки'));
-    }
-    public function profile()
-    {
-        self::role('admin');
-        Flight::render('panel/profile', array('title' => 'Панель администратора - Профиль'));
+        Flight::render('panel/index', array('title' => 'Панель администратора - Главная'));
     }
 }
