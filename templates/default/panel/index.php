32f146829cf44c0f63247bbc62f5033d2ca5456b
Maksim Grishenko <endtimes104@gmail.com>
2018-10-01 19:36:38 +0300

    update index view in panel

--- templates/default/panel/index.php
+++ templates/default/panel/index.php
@@ -1,5 +1,17 @@
 <?php Flight::render('panel/layouts/header', array('title' => $title)); ?>
 <body>
+    <div class="container site-container">
 
+
+        <?php Flight::render('panel/part/site'); ?>
+
+        <?php Flight::render('panel/part/themes'); ?>
+
+        <?php Flight::render('panel/part/steam'); ?>
+
+        <?php Flight::render('panel/part/captcha'); ?>
+
+
+    </div>
 </body>
 <?php Flight::render('panel/layouts/footer'); ?>
