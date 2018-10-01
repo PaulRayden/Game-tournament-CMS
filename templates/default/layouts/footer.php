9ed398e157a948604bf16847862649369dfd0071
Maksim Grishenko <endtimes104@gmail.com>
2018-10-01 16:43:49 +0300

    update layout files

--- templates/default/layouts/footer.php
+++ templates/default/layouts/footer.php
@@ -1,6 +1,4 @@
-
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript" src="/js/main.js"></script>
+</body>
 <script type="text/javascript" src="/js/mb.js"></script>
-
+<script type="text/javascript" src="/js/main.js"></script>
 </html>
--- templates/default/layouts/header.php
+++ templates/default/layouts/header.php
@@ -8,4 +8,9 @@
 	<link rel="stylesheet" href="/css/mb.css">
 	<link rel="icon" type="image/png" href="/assets/img/favicon.png">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700&subset=cyrillic">
+    <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+    <script src="/js/jquery.js"></script>
 </head>
+<body>
+<?php Flight::render('layouts/top-menu'); ?>
+<?php Flight::render('layouts/left-menu'); ?>
