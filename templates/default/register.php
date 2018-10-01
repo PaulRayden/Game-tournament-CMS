74c89caf42125c854a93f246aa859ca955d3257c
Maksim Grishenko <endtimes104@gmail.com>
2018-10-01 16:46:42 +0300

    update markup for a login and register view

--- templates/default/register.php
+++ templates/default/register.php
@@ -29,7 +29,8 @@
     </div>
   </div>
 
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+  <script src="/js/jquery.js"></script>
   <script type="text/javascript" src="/js/main.js"></script>
   <script type="text/javascript" src="/js/mb.js"></script>
 
