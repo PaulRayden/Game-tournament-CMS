e51ef17dc1aefb27de1647705359c78e3261eb97
Maksim Grishenko <endtimes104@gmail.com>
2018-10-01 19:36:15 +0300

    added part panels

---
+++ templates/default/panel/part/site.php
@@ -0,0 +1,54 @@
+<div id="site" class="accordion md-accordion accordion-blocks mb-3" role="tablist" aria-multiselectable="true">
+    <div class="card border-0">
+        <div class="card-header panel-card-header" role="tab">
+            <a data-toggle="collapse" href="#site-accordion" aria-expanded="true">
+                <h5 class="mt-1 mb-0 color-white">
+                    <span>Настройки Сайта</span>
+                    <i class="fa fa-angle-down rotate-icon"></i>
+                </h5>
+            </a>
+        </div>
+
+        <div id="site-accordion" class="collapse show" role="tabpanel">
+            <div class="card-body">
+                <div class="card mb-lg-2">
+                    <div class="card-body">
+                        <form id="site-settings-form">
+                            <div class="row">
+                                <div class="col-md-6 mb-3">
+                                    <input class="form-control form-control-lg" type="text" placeholder="Название сайта">
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <input class="form-control form-control-lg" type="text" placeholder="Ключевые слова через запятую">
+                                </div>
+                                <div class="col-md-12 mb-3">
+                                    <div class="form-group">
+                                        <textarea class="form-control" id="exampleFormControlTextarea3" rows="5" placeholder="Описание сайта"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <form>
+                                        <div class="file-field">
+                                            <div class="mb-2">
+                                                <img width="70px" src="/assets/img/placeholder-favicon.png" class="rounded-circle z-depth-1-half avatar-pic">
+                                            </div>
+                                            <div class="d-flex justify-content-center">
+                                                <div class="btn btn-mdb-color btn-rounded">
+                                                    <span>Загрузить Favicon</span>
+                                                    <input type="file">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </form>
+                        <a id="site-settings-button" class="btn btn-indigo float-right"><i class="fa fa-save" aria-hidden="true"></i> Сохранить</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
---
+++ templates/default/panel/part/themes.php
@@ -0,0 +1,27 @@
+<div id="themes" class="accordion md-accordion accordion-blocks mb-3" role="tablist" aria-multiselectable="true">
+    <div class="card border-0">
+        <div class="card-header panel-card-header" role="tab">
+            <a data-toggle="collapse" href="#themes-accordion" aria-expanded="true">
+                <h5 class="mt-1 mb-0 color-white">
+                    <span>Настройки Темы</span>
+                    <i class="fa fa-angle-down rotate-icon"></i>
+                </h5>
+            </a>
+        </div>
+
+        <div id="themes-accordion" class="collapse show" role="tabpanel">
+            <div class="card-body">
+                <div class="card mb-lg-2">
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
