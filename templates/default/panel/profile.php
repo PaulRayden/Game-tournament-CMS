4362c9da32e33f685d5095867caf3688cd36ebcb
Maksim Grishenko <endtimes104@gmail.com>
2018-10-01 16:47:26 +0300

    del profile panel view

--- templates/default/panel/profile.php
+++
@@ -1,7 +0,0 @@
-<?php Flight::render('panel/layouts/header', array('title' => $title)); ?>
-<body>
-    <div class="container">
-
-    </div>
-</body>
-<?php Flight::render('panel/layouts/footer'); ?>
