0f098f26d1ce87738ad8109c2e3a5833b2b142c9
Maksim Grishenko <endtimes104@gmail.com>
2018-10-01 16:55:14 +0300

    del 2 panel routes

--- routes/admin/routes.php
+++ routes/admin/routes.php
@@ -1,5 +1,3 @@
 <?php
 
     Flight::route('/panel', array('Controllers\PageController','index'));
-    Flight::route('/panel/settings', array('Controllers\PageController','settings'));
-    Flight::route('/panel/profile', array('Controllers\PageController','profile'));
