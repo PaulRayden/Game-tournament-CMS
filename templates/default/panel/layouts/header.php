6b928d257728fe0924375752cb51d89c9261cf5c
Maksim Grishenko <endtimes104@gmail.com>
2018-10-01 19:35:43 +0300

    update panel layout

--- templates/default/panel/layouts/header.php
+++ templates/default/panel/layouts/header.php
@@ -4,14 +4,15 @@
 	<meta charset="UTF-8">
 	<title><?= $title ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="/css/main-panel.css">
+
 	<link rel="stylesheet" href="/css/mb.css">
+    <link rel="stylesheet" href="/css/main-panel.css">
 	<link rel="icon" type="image/png" href="/assets/img/favicon.png">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700&subset=cyrillic">
 </head>
 
-<nav class="navbar navbar-expand-lg navbar-dark indigo">
-    <a class="navbar-brand" href="/panel">Панель администратора</a>
+<nav class="navbar navbar-expand-lg navbar-dark indigo nav-panel">
+    <a class="navbar-brand" href="/panel">Панель управления</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText"
             aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -19,15 +20,75 @@
     <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="/panel">Главная</a>
+                <a class="nav-link" href="/"><i class="fa fa-arrow-left font-weight-bold" aria-hidden="true"></i>Назад</a>
             </li>
         </ul>
         <span class="navbar-text white-text">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="/logout">Выйти</a>
-            </li>
-        </ul>
+        <ul class="navbar-nav mr-auto smooth-scroll">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#site">Сайт</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#themes">Темы</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#steam">Стим</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#captcha">Капча</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/logout">Выйти</a>
+                        </li>
+                    </ul>
         </span>
     </div>
 </nav>
+
+
+
+
+
+
+<!--<!DOCTYPE html>-->
+<!--<html lang="en">-->
+<!--<head>-->
+<!--    <meta charset="UTF-8">-->
+<!--    <title>--><?//= $title ?><!--</title>-->
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1">-->
+<!--    <link rel="stylesheet" href="/css/mb.css">-->
+<!--    <link rel="stylesheet" href="/css/main-panel.css">-->
+<!--    <link rel="icon" type="image/png" href="/assets/img/favicon.png">-->
+<!--    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700&subset=cyrillic">-->
+<!--</head>-->
+<!---->
+<!--<nav class="navbar navbar-expand-lg navbar-dark indigo nav-panel">-->
+<!--    <a class="navbar-brand" href="/panel">Панель управления</a>-->
+<!---->
+<!--    <div class="collapse navbar-collapse">-->
+<!--        <ul class="navbar-nav mr-auto">-->
+<!--            <li class="nav-item">-->
+<!--                <a class="nav-link" href="/"><i class="fa fa-arrow-left font-weight-bold" aria-hidden="true"></i>Назад</a>-->
+<!--            </li>-->
+<!--        </ul>-->
+<!--        <span class="navbar-text white-text">-->
+<!--        <ul class="navbar-nav mr-auto ">-->
+<!--            <li class="nav-item">-->
+<!--                <a class="nav-link" href="#site">Сайт</a>-->
+<!--            </li>-->
+<!--            <li class="nav-item">-->
+<!--                <a class="nav-link" href="#themes">Темы</a>-->
+<!--            </li>-->
+<!--            <li class="nav-item">-->
+<!--                <a class="nav-link" href="#steam">Стим</a>-->
+<!--            </li>-->
+<!--            <li class="nav-item">-->
+<!--                <a class="nav-link" href="#captcha">Капча</a>-->
+<!--            </li>-->
+<!--            <li class="nav-item">-->
+<!--                <a class="nav-link" href="/logout">Выйти</a>-->
+<!--            </li>-->
+<!--        </ul>-->
+<!--        </span>-->
+<!--    </div>-->
+<!--</nav>-->
